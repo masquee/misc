1299feb65cbe132b05f14a8a63c7c96018c020b2
masquee <okayanz@outlook.com>
2022-09-18 22:48:37 +0800

    update

--- istio-warm-up-demo/send_request.sh
+++ istio-warm-up-demo/send_request.sh
@@ -1,1 +1,1 @@
-./fortio load -qps 200 -c 2 -t 4m -allow-initial-errors -H "Host:12345.httpbin.soa.mesh" http://1.1.1.1/get
+./fortio load -qps 200 -c 2 -t 10m -allow-initial-errors -H "Host:12345.httpbin.soa.mesh" http://1.1.1.1/get
